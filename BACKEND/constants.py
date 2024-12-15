7c8e389b5716aa8cf5e43885f37d6a0a58caa882
MUSTAFA A <mustafatinwala6@gmail.com>
2024-12-15 23:48:21 +0530

    final change

--- BACKEND/constants.py
+++ BACKEND/constants.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '5000'
-ENV = 'dev'
+SERVER_URL = '0.0.0.0'  # Render requires the app to listen on 0.0.0.0
+PORT = 5000  # Render will automatically assign the correct port through the web process
+ENV = 'prod' 
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
